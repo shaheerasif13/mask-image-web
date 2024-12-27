ae5857e8c4e88c3e5f318ec6b96c6f23af77258f
Muhammad Shaheer <shaheerasif13@outlook.com>
2024-12-27 22:05:01 +0500

    feat: Increase padding in the main container for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MaskImageCanvas from "./components/mask-image-canvas";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen min-w-full w-fit h-fit m-auto bg-gray-100 overflow-auto p-10">
+    <div className="flex justify-center items-center min-h-screen min-w-full w-fit h-fit m-auto bg-gray-100 overflow-auto p-12">
       <MaskImageCanvas />
     </div>
   );
